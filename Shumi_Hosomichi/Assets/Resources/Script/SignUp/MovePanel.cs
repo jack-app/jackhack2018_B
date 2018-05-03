ecbf5df76a504fe26bf7778aa2410508fa104f05
LAPTOP-GMPB4551\駿介 <anth1-61hkbh102@outlook.com>
2018-05-03 20:33:07 +0900

    セットアップ画面追加

---
+++ Shumi_Hosomichi/Assets/Resources/Script/SignUp/MovePanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePanel : MonoBehaviour {
+
+	// Use this for initialization
+	public void slidepanel () {
+        this.transform.localPosition += new Vector3(-1500, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
