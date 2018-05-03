82626e00054fe9825017651c1e9c5bfffc25ab98
chon-sakata <skt97yuma@outlook.jp>
2018-05-03 21:53:00 +0900

    いろいろしたけどちょっとまって

---
+++ Shumi_Hosomichi/Assets/Resources/Script/MoveShumiObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShumiObject : MonoBehaviour {
+    public Animator canvas;
+
+    public void MoveAnimation()
+    {
+        canvas.Play("shumiKiji");
+    }
+
+    public void destroy()
+    {
+        Destroy(gameObject);
+    }
+    
+}
--- Shumi_Hosomichi/Assets/Resources/Script/ShowShumi.cs
+++ Shumi_Hosomichi/Assets/Resources/Script/ShowShumi.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ShowShumi : MonoBehaviour {
     public ShumiData shumi_data;
-    public SetTextScript name;
+    public SetTextScript title;
     public SetTextScript header;
     public SetTextScript content;
+    public RectTransform contentTransform;
 
 	// Use this for initialization
 	void Start () {
-        ShumiData.ShumiRecord shumi_record = shumi_data.shumi_records[0];
-        name.SetText(shumi_record.shumi_name);
+        ShumiData.ShumiRecord shumi_record = shumi_data.shumi_records[2];
+        title.SetText(shumi_record.shumi_name);
         header.SetText(shumi_record.shumi_header);
         content.SetText(shumi_record.shumi_content);
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x, 220 + contentTransform.sizeDelta.y - contentTransform.localPosition.y);
 	}
 }
