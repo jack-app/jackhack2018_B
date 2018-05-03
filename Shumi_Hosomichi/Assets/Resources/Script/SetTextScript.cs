71607fa4704290b62a9576dcf22f30e2e789a849
chon-sakata <skt97yuma@outlook.jp>
2018-05-03 17:19:49 +0900

    テキスト適用準備

---
+++ Shumi_Hosomichi/Assets/Resources/Script/SetTextScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextScript : MonoBehaviour {
+
+	public void SetText(TextAsset asset)
+    {
+        gameObject.GetComponent<Text>().text = asset.text;
+    }
+
+    public void SetText(string text)
+    {
+        gameObject.GetComponent<Text>().text = text;
+    }
+}
---
+++ Shumi_Hosomichi/Assets/Resources/Script/ShowShumi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowShumi : MonoBehaviour {
+    public ShumiData shumi_data;
+    public SetTextScript name;
+    public SetTextScript header;
+    public SetTextScript content;
+
+	// Use this for initialization
+	void Start () {
+        ShumiData.ShumiRecord shumi_record = shumi_data.shumi_records[0];
+        name.SetText(shumi_record.shumi_name);
+        header.SetText(shumi_record.shumi_header);
+        content.SetText(shumi_record.shumi_content);
+	}
+}
