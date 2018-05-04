633187131227e0e3348217c4b2d04291eef3d6b3
LAPTOP-GMPB4551\駿介 <anth1-61hkbh102@outlook.com>
2018-05-04 19:15:37 +0900

    サインアップデザイン等

---
+++ Shumi_Hosomichi/Assets/Resources/Script/ChangetoChat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangetoChat : MonoBehaviour {
+
+	// Use this for initialization
+	public void SceneLoad () {
+        Application.LoadLevel("Chat");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
