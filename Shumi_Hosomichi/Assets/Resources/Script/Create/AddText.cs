e3653c7ce116b98f92da0bb8732e5f341ccffb55
chon-sakata <skt97yuma@outlook.jp>
2018-05-04 19:58:11 +0900

    CreateMenuを追加

---
+++ Shumi_Hosomichi/Assets/Resources/Script/Create/AddText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddText : MonoBehaviour {
+    public void AddHeadLine()
+    {
+        GetComponent<InputField>().text += "<>";
+        //GetComponent<InputField>().ActivateInputField();
+        //GetComponent<InputField>().MoveTextEnd(false);
+        //GetComponent<InputField>().caretPosition--;
+    }
+	
+}
---
+++ Shumi_Hosomichi/Assets/Resources/Script/Create/TransPreview.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransPreview : MonoBehaviour {
+
+	public void GoPreview()
+    {
+        GetComponent<RectTransform>().localPosition += new Vector3(-1500, 0, 0);
+    }
+
+    public void BackPreview()
+    {
+        GetComponent<RectTransform>().localPosition += new Vector3(1500, 0, 0);
+    }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene("shumi_menu");
+    }
+}
