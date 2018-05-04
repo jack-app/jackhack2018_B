f10826ccae36dcd982c39945dd43d5280c55dd7f
shiro0309 <eri.a.ran.0309@outlook.jp>
2018-05-04 19:00:22 +0900

    チャット場面。新しいメッセージに対するスクロールが上手くいかない状態。

---
+++ Shumi_Hosomichi/Assets/Resources/Script/Chat.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Chat : MonoBehaviour {
+	//要素を追加するコンテント
+	[SerializeField] RectTransform content;
+
+	//生成する要素
+	[SerializeField] RectTransform originalElement;
+	[SerializeField] Text elementOriginalText;
+ 	[SerializeField] RectTransform botElement;
+	[SerializeField] Text elementbotText;
+private int i;
+private string bot;
+public ScrollRect ScrollRect;
+
+	// テキスト入力フィールド
+	[SerializeField] InputField input;
+	private string[] bottext={"いつもありがとうございます。","ただいま対応しておりません。","またの機会にご利用ください。"};
+	void Awake()
+	{
+		//（shiro）最初は何も表示されていないようにする
+		originalElement.gameObject.SetActive (false);
+		botElement.gameObject.SetActive(false);
+	}
+
+	public void OnSubmit()
+	{
+		// 入力フィールドを元に複製元のデータを改変
+		// 入力フィールドは初期化する
+		elementOriginalText.text = input.text;
+		input.text = string.Empty;
+		i=Random.Range(0,3);
+		bot = bottext[i];
+		elementbotText.text = bot;
+
+
+
+		// content以下にoriginalElementを複製
+		var element = GameObject.Instantiate<RectTransform> (originalElement);
+		//（shiro）contentをelementの親にする
+		element.SetParent (content, false);
+		//（shiro）最後に表示されるようにする
+		element.SetAsLastSibling ();
+		//（shiro）画面上に表示する
+		element.gameObject.SetActive (true);
+
+
+		var botelement = GameObject.Instantiate<RectTransform>(botElement);
+		botelement.SetParent (content, false);
+		botelement.SetAsLastSibling ();
+		botelement.gameObject.SetActive (true);
+		ScrollRect.verticalNormalizedPosition = 0; //ここでスクロールを一番下にする
+
+
+
+	}
+}
