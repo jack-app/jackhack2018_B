1e520c4b845e811624b2a5d8de1f10f67b4c44b5
chon-sakata <skt97yuma@outlook.jp>
2018-05-04 13:55:45 +0900

    Mainシーンがある程度完成いたしました。

--- Shumi_Hosomichi/Assets/Resources/Script/Main/ShowShumi.cs
+++ Shumi_Hosomichi/Assets/Resources/Script/Main/ShowShumi.cs
@@ -7,14 +7,23 @@ public class ShowShumi : MonoBehaviour {
     public SetTextScript title;
     public SetTextScript header;
     public SetTextScript content;
+    public SetImageScript topImage;
     public RectTransform contentTransform;
+    static int shumi_number = 0;
 
 	// Use this for initialization
 	void Start () {
-        ShumiData.ShumiRecord shumi_record = shumi_data.shumi_records[2];
+        ShumiData.ShumiRecord shumi_record = shumi_data.shumi_records[shumi_number];
         title.SetText(shumi_record.shumi_name);
         header.SetText(shumi_record.shumi_header);
         content.SetText(shumi_record.shumi_content);
-        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x, 220 + contentTransform.sizeDelta.y - contentTransform.localPosition.y);
+        topImage.SetImage(shumi_record.shumi_image);
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x, 430 + contentTransform.sizeDelta.y - contentTransform.localPosition.y);
+        shumi_number++;
 	}
+
+    public int GetShumiNumber()
+    {
+        return shumi_number;
+    }
 }
